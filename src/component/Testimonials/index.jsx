4b4727161d45b9ab63958aee3630d953fdb17fe9
Bishownath Dhakal <b_dhakal186428@fanshaweonline.ca>
2023-10-12 00:31:43 -0400

    testimonials complete and working on work portion of the portofolio

--- src/component/Testimonials/index.jsx
+++ src/component/Testimonials/index.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import "./styles.scss"
 export default function Testimonials() {
+
+    const testimonialsData = [
+        {
+            id: 1,
+            name: "Soobas",
+            title: "Software Developer",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ut repellendus sapiente rerum provident modi, doloribus reiciendis assumenda.",
+            img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=800",
+            hovered: false
+        },
+        {
+            id: 2,
+            name: "Bishow",
+            title: "Sr. Frontend Developer",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ut repellendus sapiente rerum provident modi, doloribus reiciendis assumenda.",
+            img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=800",
+            hovered: true
+        },
+        {
+            id: 1,
+            name: "Soobas",
+            title: "CEO",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ut repellendus sapiente rerum provident modi, doloribus reiciendis assumenda.",
+            img: "https://images.pexels.com/photos/91224/pexels-photo-91224.jpeg?auto=compress&cs=tinysrgb&w=800",
+            hovered: false
+        },
+    ]
     return (
         <div className='testimonials' id='testimonials'>
+            <h1>Testimonials</h1>
+            <div className="container">
+                {testimonialsData.map(td => (
+                    <div className={td.hovered ? 'card hovered' : 'card'}>
+                        <div className="top">
+                            <img src="https://cdn-icons-png.flaticon.com/128/2268/2268536.png" alt="" className='left' />
+                            <img src={td.img} alt="" className='userImage' />
+                            <img src="https://cdn-icons-png.flaticon.com/128/1968/1968750.png" alt="" className='right' />
+                        </div>
+                        <div className="center">
+                            {td.desc}
+                        </div>
+                        <div className="bottom">
+                            <h1>{td.name}</h1>
+                            <h3>{td.title}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/component/Work/index.jsx
+++ src/component/Work/index.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import "./styles.scss"
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Work() {
+
+    const itemData = [
+        {
+            id: 1,
+            title: "Cello",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+            img: "https://images.pexels.com/photos/54284/pexels-photo-54284.jpeg?auto=compress&cs=tinysrgb&w=800"
+        },
+        // {
+        //     id: 2,
+        //     title: "Hello World 2",
+        //     description: "Lorem Ipsum 2",
+        //     // icon: ''
+        // },
+        // {
+        //     id: 3,
+        //     title: "Hello World 3",
+        //     description: "Lorem Ipsum 3",
+        //     // icon: ''
+        // },
+    ]
     return (
         <div className='work' id='work'>
+            {/* <h3>Works</h3> */}
+            <div className="slider">
+                {itemData.map(items => (
+
+
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <img src="https://images.pexels.com/photos/54284/pexels-photo-54284.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
+                                <h1>{items.title}</h1>
+                                <h3>{items.description}</h3>
+                                <a href='#'><GitHubIcon /></a>
+                            </div>
+                            <div className="right">
+                                <img src="assets/pickachu.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
+
     )
 }
